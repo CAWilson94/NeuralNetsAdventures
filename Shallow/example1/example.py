f266853660317886235d2d50660d424f14075b19
PurpleCoffee <rmb12134@strath.ac.uk>
2015-11-24 21:50:57 +0000

    import dataset, split into testing and training data

--- Shallow/example1/example.py
+++ Shallow/example1/example.py
@@ -4,10 +4,23 @@ Created November 2015
 
 """
 
-#Import packages needed
+# Import packages needed
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
 from sklearn import datasets
 from nolearn.dbn import DBN
 import numpy as np
-import cv3 
+import cv2
+
+# Use the MNIST dataset
+print "[X]downloading dataset..."
+dataset = datasets.fetch_mldata("MNIST Original")
+
+"""
+Generate training and testing splits!
+"""
+
+# scale the data to range [0,1] 
+# construct training and testing split
+(trainX, testX, trainY, testY) = train_test_split(
+    dataset.data / 255.0, dataset.target.astype("int0"), test_size = 0.33)
