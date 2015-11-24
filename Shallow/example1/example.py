7a5dcac969186b0abfc8506b47ee10bb3f236f61
PurpleCoffee <rmb12134@strath.ac.uk>
2015-11-24 22:00:05 +0000

    training dataset

--- Shallow/example1/example.py
+++ Shallow/example1/example.py
@@ -22,5 +22,19 @@ Generate training and testing splits!
 
 # scale the data to range [0,1] 
 # construct training and testing split
+# 33% data for testing remaining 67% for training
 (trainX, testX, trainY, testY) = train_test_split(
-    dataset.data / 255.0, dataset.target.astype("int0"), test_size = 0.33)
+    dataset.data / 255.0, dataset.target.astype("int0"), test_size=0.33)
+
+"""
+Train the network with 784 inputs (flattened 28x28 greyscale image)
+300 hidden units, 10 output units: 1 for each possible output
+classification,which are the digits 1-10)
+"""
+dbn = DBN(
+    [trainX.shape[1], 300, 10],
+    learn_rates= 0.3,
+    learn_rate_decays= 0.9,
+    epochs =10,
+    verbose =1)
+dbn.fit(trainX, trainY)
