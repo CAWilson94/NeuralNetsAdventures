25c063ea5b4a4fd6b2e07d9867495b855e25fa3f
PurpleCoffee <rmb12134@strath.ac.uk>
2015-11-26 12:07:58 +0000

    fitting and evaluation tried out, fix training test split

--- shallow_1/shallowBC.py
+++ shallow_1/shallowBC.py
@@ -17,6 +17,12 @@ The number of outputs will be 6 for the 6 different defect types.
 '''
 import pandas as pd 
 import numpy as np
+# Neural nets model inputs
+from nolearn.dbn import DBN 
+import cv2
+# Scikit learn imports
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
 
 print "hello"
 
@@ -24,15 +30,51 @@ print "hello"
 #---------------------
 train_data = pd.read_csv(open('BC27.L3D.csv'))
 
-#Generate training data
-#----------------------
+# Generate training data
+#-----------------------
 
 # Convert into pandas data frame
-train = pd.DataFrame(train_data)
+dataset = pd.DataFrame(train_data)
 print "panda data frame"
 # Get data into a bunch object
-type(train)
+type(dataset)
 print "bunches"
+
+# Training the network
+#---------------------
+
+"""
+Train the network with 3200 inputs (64x50 values in file)
+6 output units (for the different defects)
+Lets start with 2 hidden units
+"""
+
+# Numbers from example used here
+(trainX, testX, trainY, testY) = train_test_split(
+    dataset / 255.0, dataset.target.astype("int0"), test_size=0.33)
+
+dbn = DBN(
+    # [[numNodes input layer], numNodes hidden layer, numNodes output layer ]
+    [trainX.shape[1], 2, 6],
+    # Learning rate of algorithm
+    learn_rates=0.3,
+    # Decay of learn rate
+    learn_rate_decays=0.9,
+    # Iterations of training data (epochs)
+    epochs=10,
+    # Verbosity level
+    verbose=1)
+dbn.fit(trainX,trainY) 
+
+print "trained yo!"
+
+# Evaluate network
+#-----------------
+preds = dbn.predict(testX)
+print classification_report(testY, preds) # Table of accuracies 
+
+
+
 
 
 
